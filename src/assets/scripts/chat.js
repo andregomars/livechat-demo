979d3197e82f03127ac0bf9bc07981fc08416a4a
andre <andregomars@gmail.com>
2018-06-01 15:48:35 -0700

    update chat.js

--- src/assets/scripts/chat.js
+++ src/assets/scripts/chat.js
@@ -1,6 +1,9 @@
-﻿
-window.__lc = window.__lc || {};
+﻿window.__lc = window.__lc || {};
 window.__lc.license = 9808415; // andre.shen@papermart.com
+window.__lc.params = [
+    { name: 'phone', value: '6262224444' },
+    { name: 'company', value: 'blizzard' }
+];
 (function () {
     var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
     lc.src = ('https:' === document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
